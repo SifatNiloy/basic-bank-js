981a0939de7d55c125af2eff18a9a8ce01da92d8
SifatNiloy <sifatniloy18@gmail.com>
2021-08-15 21:21:54 +0600

    first commit

---
+++ login.js
@@ -0,0 +1,10 @@
+document.getElementById('login-submit').addEventListener('click', function () {
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    //get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    if (userEmail == "sifat@gmail.com" && userPassword == "1234") {
+        window.location.href = "banking.html";
+    }
+})
